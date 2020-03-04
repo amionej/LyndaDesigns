45fbc5a1301b8540e3bb65c49180ca9cf049bd71
Jorge Amione <jorgeamione96@gmail.com>
2020-03-04 11:34:17 -0600

    React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { ApolloProvider } from '@apollo/react-hooks';
 import client from './api/ApolloClient';
+import Homepage from './components/HomePage';
 import './App.css';
 
+const history = createBrowserHistory();
+
 const App = () => {
-  return <ApolloProvider client={client}>App</ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        {/* <AppBar />> */}
+        <main>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+          </Switch>
+          {/* <Fotter /> */}
+        </main>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
 export default App;
