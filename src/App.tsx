65fda62ff99ea3997483476903ae0148bc980e20
Carlos <carlosfgarza@gmail.com>
2020-05-05 23:29:27 -0500

    landing page update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ApolloProvider } from '@apollo/react-hooks';
-import './App.css';
+import './css/App.css';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Modal from 'react-modal';
-import './Catalog.css';
+import '../css/Catalog.css';
 
 const useStyles = makeStyles(theme => ({
   heroContent: {
